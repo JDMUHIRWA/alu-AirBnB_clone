d0a8a0841cfe6fcc59172c26cac9c8f6a40a6441
JDMUHIRWA <j.hareriman@alustudent.com>
2024-09-10 16:49:44 +0200

    `refactor: Update file_storage module docstring formatting`

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,9 +5,7 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """
-    This class represents a file storage system for storing and retrieving objects.
-    """
+    #This class represents a file storage system for storing and retrieving objects
 
     __file_path = "file.json"
     __objects = {}
