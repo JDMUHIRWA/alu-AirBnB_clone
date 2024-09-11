d267506bd76fa217fac89e07ebb7ea2e7f67c3da
JDMUHIRWA <j.hareriman@alustudent.com>
2024-09-11 18:54:41 +0200

    `refactor: Update BaseModel constructor to handle datetime values in kwargs`

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == __class__:
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(val, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
